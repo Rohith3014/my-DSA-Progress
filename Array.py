46a0aa9ba720edcb7ff07d637f07ae1c43c7a39a
Rohith <rohithhc62@gmail.com>
2026-04-24 21:14:55 +0530

    Array

--- Array.py
+++ Array.py
@@ -1,20 +1,9 @@
-
-n = int(input("Enter number of elements: "))
-
-arr = []
-
-print("Enter elements:")
-for i in range(n):
-    val = int(input())
-    arr.append(val)
-
+arr = [10, 25, 5, 40, 15]
 
 max_val = arr[0]
-
 
 for i in arr:
     if i > max_val:
         max_val = i
-
 
 print("Maximum element =", max_val)
