7a576f023d61f3da16836078904d3f0b8b00c807
Rohith <rohithhc62@gmail.com>
2026-05-18 23:19:07 +0530

    dictionary topics

--- Dictionary.py
+++ Dictionary.py
@@ -1,49 +1,3 @@
-# 1. Count Frequency
-data = ["apple", "banana", "apple", "mango"]
-
-count = {}
-
-for item in data:
-    count[item] = data.count(item)
-
-print("Frequency:")
-print(count)
-
-
-# 2. First Non-Repeating Character
-text = "abcdabde"
-
-for ch in text:
-    if text.count(ch) == 1:
-        print("\nFirst Non-Repeating Character:")
-        print(ch)
-        break
-
-
-# 3. Group Elements By Frequency
-group = {}
-
-for item in data:
-
-    c = data.count(item)
-
-    if c not in group:
-        group[c] = []
-
-    group[c].append(item)
-
-print("\nGroup By Frequency:")
-print(group)
-
-
-# 4. Merge Two Dictionaries
-dict1 = {"a": 1}
-dict2 = {"b": 2}
-
-dict1.update(dict2)
-
-print("\nMerged Dictionary:")
-print(dict1)
 # 5. Sort Dictionary By Value
 
 data = {
@@ -101,6 +55,7 @@ invert = {}
 
 for k, v in data.items():
     invert[v] = k
+
 
 print("\nInverted Dictionary:")
 print(invert)
